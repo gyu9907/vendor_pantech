0a78ae4061b347b687d62a84925a61d46c1db790
gyu9907 <gyu9907@gmail.com>
2026-08-15 17:08:16 +0900

    ef52: use source-built sensor multihal

--- ef52/ef52-vendor-blobs.mk
+++ ef52/ef52-vendor-blobs.mk
@@ -51,7 +51,6 @@ PRODUCT_COPY_FILES += \
     vendor/pantech/ef52/proprietary/vendor/lib/libsensor_reg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsensor_reg.so \
     vendor/pantech/ef52/proprietary/vendor/lib/libmmjpeg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmjpeg.so \
     vendor/pantech/ef52/proprietary/vendor/lib/hw/camera.msm8960.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.msm8960.so \
-    vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors.msm8960.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.msm8960.so \
     vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors_native_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors_native_hal.so \
     vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors_qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors_qcom.so \
 
